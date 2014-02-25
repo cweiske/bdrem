5274821c5d92a9267ec992ec1d6757c4765ee5e8
Christian Weiske <cweiske@cweiske.de>
2014-02-25 21:11:46 +0100

    script to generate phar file

---
+++ src/phar-stub.php
@@ -0,0 +1,23 @@
+<?php
+if (!in_array('phar', stream_get_wrappers()) || !class_exists('Phar', false)) {
+    echo "Phar extension not avaiable\n";
+    exit(255);
+}
+
+$web = 'www/index.php';
+$cli = 'bin/bdrem.php';
+
+function rewritePath($path)
+{
+    if ($path == '' || $path == '/') {
+        return 'www/index.php';
+    }
+    return $path;
+}
+
+//Phar::interceptFileFuncs();
+set_include_path('phar://' . __FILE__ . PATH_SEPARATOR . get_include_path());
+Phar::webPhar(null, $web, null, array(), 'rewritePath');
+include 'phar://' . __FILE__ . '/' . $cli;
+__HALT_COMPILER();
+?>
